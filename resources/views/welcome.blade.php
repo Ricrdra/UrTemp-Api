43f22e989bb77f7453f5ebf46ce335e1bd5fb6bf
caprun <26richardr@gmail.com>
2022-03-16 20:38:34 -0600

    chore: project boilerplate and index conffig

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+
+    </style>
+</head>
+<body>
+<header>
+    <div>
+        <h1>Welcome to UTemp Api</h1>
+    </div>
+</header>
+<main>
+    <div>
+        <p>If you are student, so sorry, you're not authorized to get the data</p>
+        <p>If you are teacher, amazing! You can check the temperature of your students <a href="">Here!</a></p>
+    </div>
+</main>
+<p>One Day this will be styled, but not todaygs</p>
+<footer>
+    Powered by <a href="">@captainrun</a> && <a href="">Chems Lomito</a>
+</footer>
+
+</body>
+</html>
