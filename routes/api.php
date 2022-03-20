89772aee6fa6608a9e918735fb9f59fe4e618553
caprun <26richardr@gmail.com>
2022-03-20 10:09:25 -0600

    feat: Basic routes for cruds

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', 'AuthController@login');
-Route::post('/register', 'AuthController@register');
+//Route::post('/login', 'AuthController@login');
+//Route::post('/register', 'AuthController@register');
 
 Route::prefix('/v1')->middleware('login_required')->group(function () {
     Route::apiResource('/roles', RoleController::class);
